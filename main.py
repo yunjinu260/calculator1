5278062dc3b774af8526e2dd1bc1f0be4b22ae0c
yunjinu260 <75038wjy@naver.com>
2026-03-24 15:55:08 +0900

    Refactor main.py to be implemented in ui py and ctrl.py

--- main.py
+++ main.py
@@ -1,48 +1,17 @@
 import sys
 
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit, QHBoxLayout)
+from ui import View
+from ctrl import Control
+from PyQt5.QtWidgets import QApplication
 
-from PyQt5.QtGui import QIcon
 
-class Calculator1(QWidget):
-
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-
-    def initUI(self):
-        self.te1 = QPlainTextEdit()
-        self.te1.setReadOnly(True)
-        self.btn2=QPushButton('Clear', self)
-        self.btn2.clicked.connect(self.clearMessage)
-
-        self.btn1=QPushButton('Message', self)
-        self.btn1.clicked.connect(self.activateMessage)
-
-        hbox = QHBoxLayout()
-        hbox.addStretch(1)
-        hbox.addWidget(self.btn1)
-        hbox.addWidget(self.btn2)
-
-        vbox = QVBoxLayout()
-        vbox.addWidget(self.te1)
-        vbox.addLayout(hbox)
-        vbox.addStretch(1)
-
-        self.setLayout(vbox)
-
-        self.setWindowTitle('Calculator1')
-        self.setWindowIcon(QIcon('icon.jpg'))
-        self.resize(256,256)
-        self.show()
-    
-    def clearMessage(self):
-        self.te1.clear()
-
-    def activateMessage(self):
-        self.te1.appendPlainText("Button Clicked!")
+def main():
+    calc = QApplication(sys.argv)
+    app=QApplication(sys.argv)
+    view=View()
+    Control(view=view)
+    sys.exit(app.exec_())
+   
 
 if __name__=='__main__':
-    app = QApplication(sys.argv)
-    view = Calculator1()
-    sys.exit(app.exec_())
+    main()
