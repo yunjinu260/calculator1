3514246abe491e083bdf2b747346d67888486be1
yunjinu260 <75038wjy@naver.com>
2026-03-26 22:47:44 +0900

    Revert "Add lbl1 in ui.py to print current date"
    This reverts commit f30a8134d1173e7c2f1f27fc11d48f50e84c83f0.

--- ui.py
+++ ui.py
@@ -1,17 +1,14 @@
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit, QHBoxLayout, QLabel)
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit, QHBoxLayout)
 
 from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import QDate, Qt
 
 class View(QWidget):
 
     def __init__(self):
         super().__init__()
-        self.date = QDate.currentDate()
         self.initUI()
 
     def initUI(self):
-        self.lbl1 = QLabel(self.date.toString(Qt.DefaultLocaleLongDate), self)
         self.te1 = QPlainTextEdit()
         self.te1.setReadOnly(True)
 
@@ -26,7 +23,7 @@ class View(QWidget):
         vbox = QVBoxLayout()
         vbox.addWidget(self.te1)
         vbox.addLayout(hbox)
-        vbox.addWidget(self.lbl1)
+        vbox.addStretch(1)
 
         self.setLayout(vbox)
 
