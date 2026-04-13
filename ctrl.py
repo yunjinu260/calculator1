ecc1b93a9e0f46c243108c325e2ceac9c90c3afb
yunjinu2 <yjw9976@naver.com>
2026-04-13 22:59:42 +0900

    Add sum function in ctrl.py

--- ctrl.py
+++ ctrl.py
@@ -11,4 +11,7 @@ class Control:
         self.view.btn1.clicked.connect(self.view.activateMessage)
         self.view.btn2.clicked.connect(self.view.clearMessage)
 
+    def sum(self, a, b):
+        return a+b
+
     
