ab4201d5e5a4e338e6701b9e0019bb9f139409a2
yunjinu1 <75038wjy@naver.com>
2026-05-06 16:38:39 +0900

    Remove pow, mod function and oprerators in ui.py and ctrl.py

--- ctrl.py
+++ ctrl.py
@@ -22,12 +22,6 @@ class Control:
             elif operator =='/':
                 return f'{num1} / {num2} = {self.div(num1, num2)}'
             
-            elif operator =='^':
-                return f'{num1} ^ {num2} = {self.pow(num1, num2)}'
-            
-            elif operator =='%':
-                return f'{num1} % {num2} = {self.mod(num1, num2)}'
-            
             else:
                 return "Calculation Error"
             
@@ -59,23 +53,4 @@ class Control:
                 return e
         return a/b
 
-    def pow(self, a, b):
-        try:
-
-            if (a==0):
-                raise Exception("Base Error")
-            
-        except Exception as e:
-            return e
-        
-        return pow(a, b) 
-    
-    def mod(self, a, b):
-        try:
-            if(b==0):
-                raise Exception("Division Error")
-            
-        except Exception as e:
-            return e
-        
-        return a%b
+   
